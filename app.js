0ce1665d358984dffec2ba6d94210a8a3a20cc20
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-07-21 14:36:54 -0300

    Blank user first

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const app = Vue. createApp({ 
-    data() {
-        const res = await fetch('https://randomuser.me/api')
-        const { results } = await res.json()
-
+    data() { 
         return {
-            firstName: results[0].name.first,
-            lastName: results[0].name.last,
-            email: results[0].email,
-            gender: results[0].gender,
-            picture: results[0].picture.large
+            firstName: '',
+            lastName: '',
+            email: '',
+            gender: '',
+            picture: ''
         }
     },
     methods: {
